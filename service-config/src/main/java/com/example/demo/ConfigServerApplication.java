21088a83c6592e9a94b0e7c635379c1dc5a28c2e
huangcnajia <huangcanjia@comodin.cn>
2018-03-30 00:47:25 +0800

    t'r'w

---
+++ service-config/src/main/java/com/example/demo/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 路由网关服务
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
